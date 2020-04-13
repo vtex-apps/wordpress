24715fe79fd6557b860f8c705854aacd3d29ae33
vtex_mvandeusen <marc.vandeusen@vtex.com.br>
2020-04-13 09:35:00 -0400

    update to remove final stray characters

--- react/components/WordpressCategoryRelatedPostsBlock.tsx
+++ react/components/WordpressCategoryRelatedPostsBlock.tsx
@@ -119,7 +119,7 @@ const WordpressCategoryRelatedPostsBlock: StorefrontFunctionComponent<WPCategory
                   }}
                 />
               </Container>
-              ) : null}
+              
             </div>
           )
         )}
